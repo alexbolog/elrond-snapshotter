3109810ca6f1fe3cebd22c88e693e54b1f1b07b0
alexbolog <mbolog.alex@gmail.com>
2022-05-16 18:11:08 +0300

    first commit

---
+++ src/config.tsx
@@ -0,0 +1,6 @@
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad';
+
+export const dAppName = 'Snapshottr';
+
+export const apiAddress = 'https://api.elrond.com';
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { dAppName } from 'config';
+import { routeNames } from 'routes';
+import { getCollectionBatch, getCollectionSize } from 'apiRequests';
+
+const Home = () => {
+  const DONE_STATUS = 'Done.';
+  const [input, setInput] = React.useState('');
+  const [status, setStatus] = React.useState('Waiting for ticker');
+  const [collectionSize, setCollectionSize] = React.useState(0);
+  const [finalObj, setFinalObj] = React.useState<any[]>([]);
+  const handleDownloadBalanceJson = async () => {
+    if (finalObj.length === 0) {
+      await handleMakeSnapshot();
+    }
+    setStatus('Fetching collection size..');
+    const collSize = await fetchCollectionSize();
+    setCollectionSize(collSize);
+    const holders: { [key: string]: number } = {};
+    for (let j = 0; j < finalObj.length; j++) {
+      const crt = finalObj[j];
+      if (!holders[crt.owner]) {
+        holders[crt.owner] = 0;
+      }
+      holders[crt.owner]++;
+    }
+    await downloadFile(holders, 'holders');
+  };
+
+  const handleDownloadHoldersJson = async () => {
+    if (finalObj.length === 0) {
+      await handleMakeSnapshot();
+    }
+    setStatus('Fetching collection size..');
+    const collSize = await fetchCollectionSize();
+    setCollectionSize(collSize);
+    await downloadFile(finalObj, 'snapshot');
+  };
+
+  const handleMakeSnapshot = async () => {
+    setStatus('Fetching collection size..');
+    const collSize = await fetchCollectionSize();
+    setCollectionSize(collSize);
+    const snapshot = [];
+    for (let i = 0; i < collSize; i += 100) {
+      setStatus(`Fetching.. Progress: ${i + 100}/${collSize}`);
+      const nfts = await getCollectionBatch(input, i);
+      if (!nfts.success) {
+        window.alert('Something went wrong. Refresh the page and try again');
+        return;
+      }
+      for (let j = 0; j < nfts.data.length; j++) {
+        const crt = nfts.data[j];
+        snapshot.push(crt);
+      }
+    }
+    setFinalObj(snapshot);
+    setStatus(DONE_STATUS);
+    return null;
+  };
+
+  const fetchCollectionSize = async () => {
+    const collectionSizeResponse = await getCollectionSize(input);
+    if (collectionSizeResponse.success) {
+      return collectionSizeResponse.data;
+    }
+    return 0;
+  };
+
+  const downloadFile = async (data: any, fileName: string) => {
+    const json = JSON.stringify(data);
+    const blob = new Blob([json], { type: 'application/json' });
+    const href = await URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = href;
+    link.download = fileName + '.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className='d-flex flex-fill align-items-center container'>
+      <div className='row w-100'>
+        <div className='col-12 col-md-8 col-lg-5 mx-auto'>
+          <div className='card shadow-sm rounded p-4 border-0'>
+            <div className='card-body text-center'>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col'>
+                    <h2 className='mb-3' data-testid='title'>
+                      {dAppName}
+                    </h2>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col'>
+                    <p className='mb-3'>
+                      Input a collection ticker and get the collection snapshot!
+                      (Only works for NFTs)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <input
+                    type='text'
+                    onChange={(e) => setInput(e.target.value ?? '')}
+                  />
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <p>Current status: {status}</p>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <button
+                    className='btn btn-primary mt-3 text-white'
+                    data-testid='loginBtn'
+                    onClick={handleMakeSnapshot}
+                  >
+                    Make snapshot
+                  </button>
+                </div>
+              </div>
+              {status === DONE_STATUS && (
+                <div className='row mt-5'>
+                  <div className='col'>
+                    <button
+                      onClick={handleDownloadBalanceJson}
+                      className='btn btn-success'
+                    >
+                      Download balances json
+                    </button>
+                  </div>
+                  <div className='col'>
+                    <button
+                      onClick={handleDownloadHoldersJson}
+                      className='btn btn-success'
+                    >
+                      Download holders json
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
